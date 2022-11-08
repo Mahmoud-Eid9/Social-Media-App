a214b0c38e1163666b0250ef353e6da9a2a5304a
Mahmoud-Eid9 <62315812+Mahmoud-Eid9@users.noreply.github.com>
2022-11-08 13:29:54 +0200

    Final

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,56 +1,56 @@
-import React, { useEffect } from "react";
-import classes from "./App.module.css";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import Layout from "./containers/Layout/Layout";
-import AuthPage from "./Auth/AuthPage";
-import { connect } from "react-redux";
-import * as actions from "./Store/actions/auth";
+import React, { useEffect } from 'react';
+import classes from './App.module.css';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Layout from './containers/Layout/Layout';
+import AuthPage from './Auth/AuthPage';
+import { connect } from 'react-redux';
+import * as actions from './Store/actions/auth';
 
-const App = props => {
-    const { checkLogIn } = props;
-    useEffect(() => {
-        checkLogIn();
-    }, [checkLogIn]);
+const App = (props) => {
+  const { checkLogIn } = props;
+  useEffect(() => {
+    checkLogIn();
+  }, [checkLogIn]);
 
-    let routes = null;
+  let routes = null;
 
-    console.log(props.isLoggedIn);
+  console.log(props.isLoggedIn);
 
-    if (props.isLoggedIn) {
-        routes = (
-            <Switch>
-                <Route exact path="/home" component={Layout} />
-                <Route path="/" component={Layout} />
-                <Redirect to="/home" />
-            </Switch>
-        );
-    } else {
-        routes = (
-            <Switch>
-                <Route path="/signup" component={AuthPage} />
-                <Route path="/login" component={AuthPage} />
-                <Redirect to="/login" />
-            </Switch>
-        );
-    }
+  // if (props.isLoggedIn) {
+  routes = (
+    <Switch>
+      <Route exact path="/home" component={Layout} />
+      <Route path="/" component={Layout} />
+      <Redirect to="/home" />
+    </Switch>
+  );
+  // } else {
+  //     routes = (
+  //         <Switch>
+  //             <Route path="/signup" component={AuthPage} />
+  //             <Route path="/login" component={AuthPage} />
+  //             <Redirect to="/login" />
+  //         </Switch>
+  //     );
+  // }
 
-    return (
-        <BrowserRouter>
-            <div className={classes.App}>{routes}</div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>{routes}</div>
+    </BrowserRouter>
+  );
 };
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.auth.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.auth.isLoggedIn,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        checkLogIn: () => dispatch(actions.checkLoggedin())
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    checkLogIn: () => dispatch(actions.checkLoggedin()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
